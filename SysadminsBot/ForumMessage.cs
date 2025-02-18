fc2a2ba9f5791d9c5f133d4df5fce3c79dd293dc
Vladimir N. Korotenko <koroten@ya.ru>
2025-02-18 16:17:38 +0300

    Add service unit

--- SysadminsBot/ForumMessage.cs
+++ SysadminsBot/ForumMessage.cs
@@ -4,12 +4,19 @@ namespace SysadminsBot
 {
     public class ForumMessage
     {
+        [JsonPropertyName("author")]
         public string Author { get; set; }
+        [JsonPropertyName("mid")]
         public string Mid { get; set; }
+        [JsonPropertyName("pid")]
         public string Pid { get; set; }
+        [JsonPropertyName("body")]
         public string Body { get; set; }
+        [JsonPropertyName("date")]
         public string Date { get; set; }
+        [JsonPropertyName("title")]
         public string Title { get; set; }
+        [JsonPropertyName("url")]
         public string Url { get; set; }
         [JsonPropertyName("answer")]
         public string Answer { get; set; } = "";
